7c3c4f181f3ee1c54ec2cd276c9144ace6d4e31d
Design <globalunitadesign@google.com>
2025-09-17 17:47:23 +0900

    회사소개 한국어버전 코드 수정

--- resources/views/about/about-ko.blade.php
+++ resources/views/about/about-ko.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <iframe 
-  src="https://cubeai-about.framer.website/"   
+  src="{{ asset('about-ko.html') }}"
   style="width: 100%; height: 100vh; padding-bottom: 80px; border:none; overflow:hidden;"
   allowfullscreen
   loading="lazy">
