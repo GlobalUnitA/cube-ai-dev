c0d93a4379d89f2231d61dc99410b2a034d6b0b9
Design <globalunitadesign@google.com>
2025-09-22 11:08:05 +0900

    회사소개 베트남어버전 코드 수정

--- resources/views/about/about-vi.blade.php
+++ resources/views/about/about-vi.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <iframe 
-  src="{{ asset('about-vi.html') }}"
+  src="{{ asset('html/about-vi.html') }}"
   style="width: 100%; height: 100vh; padding-bottom: 80px; border:none; overflow:hidden;"
   allowfullscreen
   loading="lazy">
