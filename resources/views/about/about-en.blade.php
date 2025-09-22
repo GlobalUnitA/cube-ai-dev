7c77a0ed50160a19a95de6a15a9b0a895eed4751
Design <globalunitadesign@google.com>
2025-09-22 10:52:41 +0900

    회사소개 영어버전 코드 수정

--- resources/views/about/about-en.blade.php
+++ resources/views/about/about-en.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <iframe 
-  src="{{ asset('about-en.html') }}"
+  src="{{ asset('html/about-en.html') }}"
   style="width: 100%; height: 100vh; padding-bottom: 80px; border:none; overflow:hidden;"
   allowfullscreen
   loading="lazy">
