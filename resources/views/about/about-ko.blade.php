4e7bfaf9c44ca3f687ef66f7c7bb12dc99043762
Design <globalunitadesign@google.com>
2025-09-22 10:47:29 +0900

    회사소개 한국어버전 코드 수정

--- resources/views/about/about-ko.blade.php
+++ resources/views/about/about-ko.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <iframe 
-  src="{{ asset('public/about-ko.html') }}"
+  src="{{ asset('html/about-ko.html') }}"
   style="width: 100%; height: 100vh; padding-bottom: 80px; border:none; overflow:hidden;"
   allowfullscreen
   loading="lazy">
